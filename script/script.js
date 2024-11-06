9c015fcd59186c728d8ed26116eb98851b8574a8
Anthony Pizzuti <anthony.pizzuti@gmx.de>
2024-11-06 20:54:08 +0100

    favicon, icons in navbar

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
 // Animation und Einblenden des Login-Formulars nach 5 Sekunden Wartezeit + 1 Sekunde Animation
 window.addEventListener("load", () => {
-    setTimeout(() => {
-        // Login-Container und "Not a User" einblenden
-        document.getElementById("loginContainer").style.display = "block";
-        document.getElementById("notAUser").style.display = "block";
-    }, 6000); // 5 Sekunden Wartezeit + 1 Sekunde für die Animation
+  setTimeout(() => {
+    // Login-Container und "Not a User" einblenden
+    document.getElementById("loginContainer").style.display = "block";
+    document.getElementById("notAUser").style.display = "block";
+  }, 6000); // 5 Sekunden Wartezeit + 1 Sekunde für die Animation
 });
 
 // Funktion für den Gast-Login
 function guestLogin() {
-    window.location.href = "summary.html";
+  window.location.href = "summary.html";
 }
