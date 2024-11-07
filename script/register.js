a734a38d49a4da81cad923e0c8663e6d671f4671
Stephen Schaz <stephenschaz90@gmail.com>
2024-11-07 19:56:10 +0100

    register.js funktioniert, konsolentexte müssen noch ersetzt werden

--- script/register.js
+++ script/register.js
@@ -1,19 +1,21 @@
-let name = ''
+let UserDatabaseURL = "https://joinda-1dd15-default-rtdb.europe-west1.firebasedatabase.app/User/";
+
 function SignUp() {
-    //document.getElementById('wrongPwText').innerHTML = ''
+    //document.getElementById('msgBox').innerHTML = ''
     let name = document.getElementById('name').value
     let email = document.getElementById('email').value
     let password = document.getElementById('password').value
     let passwordCheck = document.getElementById('pwCheck').value
     const userRef = firebase.database().ref('User/' + name);
-    password == passwordCheck ? setUserToFirebase(userRef, name, email, password) : wrongPwCheck()
+    password == passwordCheck ? checkNewUserAvailable(userRef, name, email, password) : wrongPwCheck()
 }
 
-async function checkNewUserAvailable(name, email, password) {
+async function checkNewUserAvailable(userRef, name, email, password) {
     try {
-        
-        const snapshot = await userRef.get();
-        snapshot.exists ? userInUse() : setUserToFirebase(userRef, name, email, password);
+        const snapshot = await fetch(UserDatabaseURL +'.json')
+        const data = await snapshot.json();
+        const user = Object.values(data).find(user => user.name === name);
+        user ? userInUse():setUserToFirebase(userRef, name, email, password);
     } catch (error) {
         console.error("Fehler beim Speichern in Firebase:", error);
     }
@@ -32,9 +34,21 @@ async function setUserToFirebase(userRef, name, email, password) {
             email: email,
             password: password
         });
+        clearInput()
         //document.getElementById('msgBox').innerHTML = 'User successfully registered!';
     } catch (error) {
         console.error("Error saving user to Firebase:", error);
         //document.getElementById('msgBox').innerHTML = 'Error saving user. Please try again.';
     }
+}
+
+function clearInput() {
+    document.getElementById('name').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('password').value = ''
+    document.getElementById('pwCheck').value = ''
+}
+
+function userInUse() {
+    console.log('Benutzer besteht bereits')
 }
