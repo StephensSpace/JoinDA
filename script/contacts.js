90ba1aa48b4f8bfc692d520f209e1af0c41704f8
Bormex <siilex13@gmail.com>
2024-11-12 18:53:30 +0100

    Update - Contacts.js

--- script/contacts.js
+++ script/contacts.js
@@ -5,9 +5,17 @@
 
 //############################################################
 
+async function getFirebaseData(path = "/") {
+    const SNAPSHOT = await firebase.database().ref(path).once('value');   //   .ref("\User/Niclas")
+    const RESULT = SNAPSHOT.val(); // Ergebnis als Object
+    return RESULT;
+}
 
-
-
+async function getContactsLength() {
+    const ALL_CONTACTS = (await getFirebaseData(`contacts/`));   
+    const LENGTH_OF_ALL_CONTACTS = Object.keys(ALL_CONTACTS).length;
+    return LENGTH_OF_ALL_CONTACTS;
+}
 
 
 
@@ -18,34 +26,22 @@
 
 //############################################################
 
-async function getFirebaseData(path = "/") {
-    const SNAPSHOT = await firebase.database().ref(path).once('value');   //   .ref("\User/Niclas")
-    const RESULT = SNAPSHOT.val(); // Ergebnis als Object
-    return RESULT;
+async function getAllDetailsOfEachUser() {
+  const OBJECT = await getFirebaseData(path = "/contacts");   // OBJECT MIT ALLEN USERN
+
+  for (let userIndex = 0; userIndex <= await getContactsLength(); userIndex++) { // 8 noch ersetzten durch object länge
+    
+    const USER = Object.keys(OBJECT)[userIndex];   // iteriert durch die User in "Contacts"
+
+    const USER_NAME = (await getFirebaseData(`contacts/${USER}`)).name;
+    const USER_EMAIL = (await getFirebaseData(`contacts/${USER}`)).email;
+    const USER_PHONE_NUMB = (await getFirebaseData(`contacts/${USER}`)).phone_number;
+
+
+    return {USER_NAME, USER_EMAIL, USER_PHONE_NUMB}
   }
-  
-  async function getContactsLength() {
-    const ALL_CONTACTS = (await getFirebaseData(`contacts/`));   
-    const LENGTH_OF_ALL_CONTACTS = Object.keys(ALL_CONTACTS).length;
-    return LENGTH_OF_ALL_CONTACTS;
-  }
-  
-  async function getAllDetailsOfEachUser() {
-    const OBJECT = await getFirebaseData(path = "/contacts");   // OBJECT MIT ALLEN USERN
-  
-    for (let userIndex = 0; userIndex <= await getContactsLength(); userIndex++) { // 8 noch ersetzten durch object länge
-      
-      const USER = Object.keys(OBJECT)[userIndex];   // iteriert durch die User in "Contacts"
-  
-      const USER_NAME = (await getFirebaseData(`contacts/${USER}`)).name;
-      const USER_EMAIL = (await getFirebaseData(`contacts/${USER}`)).email;
-      const USER_PHONE_NUMB = (await getFirebaseData(`contacts/${USER}`)).phone_number;
-  
-  
-      return {USER_NAME, USER_EMAIL, USER_PHONE_NUMB}
-    }
-  
-  }
+
+}
   
   
   
