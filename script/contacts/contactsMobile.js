a7baca4bf82d419d27d294fb6816eb2d5462c3a4
Alina Brennecke <alinabrennecke@Alinas-MacBook-Pro.local>
2024-12-05 17:17:31 +0100

    update

--- script/contacts/contactsMobile.js
+++ script/contacts/contactsMobile.js
@@ -2,13 +2,14 @@
 // Zum zurück kommen vom Contact Table zur Contact List
 function goBackToContactList() {
     document.getElementsByClassName('contact-board')[0].style.display = "flex";
-    document.getElementById('contact-content-table').style.display = "none";
+    document.getElementsByClassName('contact-content')[0].style.display = "none";
+    document.getElementById('contact-content-table').innerHTML = "";
 }
 
 
 // Funktion zum rendern der User Details ins Contact Content Table
 function renderContactInfosInContactsTableMobile(index) {
-    document.getElementById('contact-content-table').style.display = "block";
+    document.getElementsByClassName('contact-content')[0].style.display = "flex";
     document.getElementsByClassName('contact-board')[0].style.display = "none";
     renderContactInfosInContactsTable(index);
 }
