36febeb60ec0654607f7f08abf1e4ca879134208
Alina Brennecke <alinabrennecke@Alinas-MacBook-Pro.local>
2024-12-06 11:30:32 +0100

    update

--- script/contacts/contactsMobile.js
+++ script/contacts/contactsMobile.js
@@ -4,6 +4,9 @@ function goBackToContactList() {
     document.getElementsByClassName('contact-board')[0].style.display = "flex";
     document.getElementsByClassName('contact-content')[0].style.display = "none";
     document.getElementById('contact-content-table').innerHTML = "";
+
+    document.getElementsByClassName('add-new-contact-background')[1].remove();  //   classe wird noch geändert!!
+
 }
 
 
@@ -12,4 +15,12 @@ function renderContactInfosInContactsTableMobile(index) {
     document.getElementsByClassName('contact-content')[0].style.display = "flex";
     document.getElementsByClassName('contact-board')[0].style.display = "none";
     renderContactInfosInContactsTable(index);
+
+
+    document.getElementsByClassName('content')[0].innerHTML +=      // classe wird noch geändert!!
+    `<div class="add-new-contact-background">
+            <div class="add-new-contact-btn" onclick="">
+                <p>...</p>
+            </div>
+    </div>`;
 }
